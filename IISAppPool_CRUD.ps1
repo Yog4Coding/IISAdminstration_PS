bc341942ef550269bc96f2399986e84ff096bbe6
Yog4Coding <58214287+Yog4Coding@users.noreply.github.com>
2020-04-26 15:58:07 +0530

    Add files via upload

---
+++ IISAppPool_CRUD.ps1
@@ -0,0 +1,23 @@
+﻿# Basics Of IIS App Pool #
+# CRUD Operation #
+Import-Module WebAdministration
+
+Get-Command -Name *apppool*
+
+# List IIS App Pools 
+Get-ChildItem -Path IIS:\AppPools
+
+(Get-Item -Path 'IIS:\AppPools\DefaultAppPool').userName
+
+# Create App Pool
+New-WebAppPool -Name "Learning1" -Verbose -Force
+
+# Read All Properties
+(Get-Item -Path 'IIS:\AppPools\Learning1').CPU.attributes | Select-Object name,value
+(Get-Item -Path 'IIS:\AppPools\Learning1').Recycling.attributes | Select-Object name,value
+
+# Updated Properties
+Set-ItemProperty 'IIS:\AppPools\Learning1' -name "CPU" -value @{limit = 70000; action= "ThrottleUnderLoad";}
+
+# Delete IIS App Pool
+#Remove-WebAppPool "Learning1"
---
+++ IISWebSite_CRUD.ps1
@@ -0,0 +1,50 @@
+﻿# Basics Of IIS WebSite #
+# CRUD Operation #
+
+
+Function Create-Directory {
+ param ([string]$DirPath  )
+
+ if(!(Test-Path $DirPath))
+    {
+        New-Item -ItemType Directory -Path $DirPath
+    }
+
+}
+
+Import-Module WebAdministration
+
+#Get-Command -Name *web*
+
+# Setting all Prerequsities 
+$DemoSite = "C:\DemoSite"
+$DemoApp = "$DemoSite\DemoApp"
+$DemoVD1 = "$DemoSite\DemoVirtualDir1"
+$DemoVD2 = "$DemoSite\DemoVirtualDir2"
+$PORT = "4200"
+
+Create-Directory $DemoSite
+Create-Directory $DemoApp
+Create-Directory $DemoVD1
+Create-Directory $DemoVD2
+
+
+# Create new App Pool
+if((Test-Path 'IIS:\AppPools\DemoAppPool'))
+{
+    Remove-Item IIS:\AppPools\DemoAppPool -Force
+}
+New-Item IIS:\AppPools\DemoAppPool
+
+# Create New Sites, Web Applications and Virtual Directories and Assign to Application Pool
+New-Item IIS:\Sites\DemoSite -physicalPath $DemoSite -ApplicationPool "DemoAppPool" -Id $PORT -bindings @{protocol="https";bindingInformation=":${PORT}:"}
+Set-ItemProperty IIS:\Sites\DemoSite -name applicationPool -value DemoAppPool
+
+New-Item IIS:\Sites\DemoSite\DemoApp -physicalPath $DemoApp -type Application
+Set-ItemProperty IIS:\sites\DemoSite\DemoApp -name applicationPool -value DemoAppPool
+New-Item IIS:\Sites\DemoSite\DemoVirtualDir1 -physicalPath $DemoVD1 -type VirtualDirectory
+New-Item IIS:\Sites\DemoSite\DemoApp\DemoVirtualDir2 -physicalPath $DemoVD2 -type VirtualDirectory
+
+
+#Delete All above
+#Remove-Website "DemoSite"
